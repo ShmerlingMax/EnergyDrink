f5ee289b50e836b4261d73e1fd629f251e715b5b
Максим Шмерлинг <shmerling@inbox.ru>
2022-12-13 20:04:29 +0300

    Make a list item display

--- mobile_app/lib/presentation/main_screen/widgets/item.dart
+++ mobile_app/lib/presentation/main_screen/widgets/item.dart
@@ -2,15 +2,16 @@ import 'package:energy_drink/domain/models/energy_drink_model/energy_drink_model
 import 'package:flutter/material.dart';
 
 class Item extends StatelessWidget {
-  final EnergyDrink energyDrink;
+  final MapEntry<EnergyDrink, Image> energyDrink;
   final int index;
 
   const Item(this.energyDrink, this.index, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final itemWidth = MediaQuery.of(context).size.width / 3;
+    final itemHeight = MediaQuery.of(context).size.height / 4.7;
     return Container(
-      height: 300,
       decoration: BoxDecoration(
         border: Border(
           bottom: const BorderSide(
@@ -25,8 +26,63 @@ class Item extends StatelessWidget {
               : const BorderSide(color: Colors.black, width: 0.5),
         ),
       ),
-      child: Center(
-        child: Text(energyDrink.fullName),
+      child: Column(
+        children: [
+          SizedBox(
+            width: itemWidth,
+            height: itemHeight,
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: energyDrink.key.image,
+            ),
+          ),
+          Text(energyDrink.key.fullName),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text('${energyDrink.key.priceWithDiscount} ₽'),
+              Text('${energyDrink.key.volume} мл'),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                '${energyDrink.key.priceWithOutDiscount} ₽',
+                style: const TextStyle(decoration: TextDecoration.lineThrough),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                '${(energyDrink.key.discount * 100).round()}% OFF',
+                style: const TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+              const Spacer(),
+              SizedBox(
+                width: 50,
+                height: 30,
+                child: energyDrink.value,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
       ),
     );
   }
